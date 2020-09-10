0b5533e8db59d42282ca7c165975f572de34b67b
David Berri <dwbwill@gmail.com>
2020-09-10 18:09:21 -0300

    Adds data to the info strip dynamically

--- src/components/InfoStrip/InfoStrip.tsx
+++ src/components/InfoStrip/InfoStrip.tsx
@@ -1,41 +1,38 @@
 import React from 'react'
 import './InfoStrip.css';
 
+const data = [
+  {
+    label: "IP Address",
+    value: "192.212.174.101"
+  },
+  {
+    label: "Location",
+    value: "Broolyn, NY 10001"
+  },
+  {
+    label: "Timezone",
+    value: "UTC -05:00"
+  },
+  {
+    label: "ISP",
+    value: "SpaceX Starlink"
+  },
+]
+
 const InfoStrip = () => {
   return (
     <div className="InfoStrip">
-      <div className="InfoStrip-data">
-        <h2 className="InfoStrip-dataTitle">
-          IP Address
-        </h2>
-        <p className="InfoStrip-dataParagraph">
-          Test
-        </p>
-      </div>
-      <div className="InfoStrip-data">
-        <h2 className="InfoStrip-dataTitle">
-          Location
-        </h2>
-        <p className="InfoStrip-dataParagraph">
-          Test
-        </p>
-      </div>
-      <div className="InfoStrip-data">
-        <h2 className="InfoStrip-dataTitle">
-          Timezone
-        </h2>
-        <p className="InfoStrip-dataParagraph">
-          Test
-        </p>
-      </div>
-      <div className="InfoStrip-data">
-        <h2 className="InfoStrip-dataTitle">
-          ISP
-        </h2>
-        <p className="InfoStrip-dataParagraph">
-          Test
-        </p>
-      </div>
+      {data.map(info => (
+        <div className="InfoStrip-data">
+          <h2 className="InfoStrip-dataTitle">
+            {info.label}
+          </h2>
+          <p className="InfoStrip-dataParagraph">
+            {info.value}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
