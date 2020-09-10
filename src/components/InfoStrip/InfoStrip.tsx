6afc3adddefe1c0fbf4b3215a38cf7db4dba81d6
David Berri <dwbwill@gmail.com>
2020-09-10 18:15:51 -0300

    Passes data to InfoStrip with props

--- src/components/InfoStrip/InfoStrip.tsx
+++ src/components/InfoStrip/InfoStrip.tsx
@@ -1,38 +1,30 @@
 import React from 'react'
 import './InfoStrip.css';
 
-const data = [
-  {
-    label: "IP Address",
-    value: "192.212.174.101"
-  },
-  {
-    label: "Location",
-    value: "Broolyn, NY 10001"
-  },
-  {
-    label: "Timezone",
-    value: "UTC -05:00"
-  },
-  {
-    label: "ISP",
-    value: "SpaceX Starlink"
-  },
-]
+interface Info {
+  label: string,
+  value: string,
+}
 
-const InfoStrip = () => {
+interface Props {
+  data: Info[]
+}
+
+const InfoStrip: React.FC<Props> = ({ data }) => {
+  const info = data.map(info => (
+    <div className="InfoStrip-data">
+      <h2 className="InfoStrip-dataTitle">
+        {info.label}
+      </h2>
+      <p className="InfoStrip-dataParagraph">
+        {info.value}
+      </p>
+    </div>
+  ));
+
   return (
     <div className="InfoStrip">
-      {data.map(info => (
-        <div className="InfoStrip-data">
-          <h2 className="InfoStrip-dataTitle">
-            {info.label}
-          </h2>
-          <p className="InfoStrip-dataParagraph">
-            {info.value}
-          </p>
-        </div>
-      ))}
+      {info}
     </div>
   );
 }
